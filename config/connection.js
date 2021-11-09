cb88f002091e2135e709e78b889ffd0bdbef5bfa
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-11-09 15:45:09 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // import the sequalize constructor from the library
-const Sequalize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to database, pass in mysql information
-const sequalize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
 
-module.exports = sequalize;
+module.exports = sequelize;
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create user model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+       // define an id column
+       id: {
+           type: DataTypes.INTEGER,
+           allowNull: false,
+           primaryKey: true,
+           autoIncrement: true
+       },
+       // define username column
+       username: {
+           type: DataTypes.STRING,
+           allowNull: false
+       },
+       // define an email column
+       email: {
+           type: DataTypes.STRING,
+           allowNull: false,
+           unique: true,
+           validate: {
+               isEmail: true
+           }
+       },
+       // define a password column
+       password: {
+           type: DataTypes.STRING,
+           allowNull: false,
+           validate: {
+               len: [4]
+           }
+       }
+    },
+    {
+        // TABLE CONFIGURATION OPTIONS GO HERE
+
+       // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'
+    }
+);
+
+module.exports = User;
