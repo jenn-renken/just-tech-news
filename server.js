791244ed4d37ae36a6c8562169bdec334cf92d4d
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-11-12 12:41:52 -0500

    added vote routes, refactored code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const { truncateSync } = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
